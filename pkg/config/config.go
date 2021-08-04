7a543d0f49006df4729651aa2dde16d9a51ecb50
Artem Nikolaiev <artyom.nikolaev@yahoo.com>
2021-08-04 21:03:58 +0300

    feat(config): add workspace file, implement config pkg

---
+++ pkg/config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/iamolegga/enviper"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Production bool `mapstructure:"production"`
+
+	// logger
+	LogLevel    string `mapstructure:"log_level"`
+	LogResponse bool   `mapstructure:"mysql_database"`
+
+	// base url
+	BaseURL string `mapstructure:"base_url"`
+	Port    string `mapstructure:"port"`
+
+	// MySQL
+	MySQLHost     string `mapstructure:"mysql_host"`
+	MySQLUser     string `mapstructure:"mysql_user"`
+	MySQLPass     string `mapstructure:"mysql_pass"`
+	MySQLDatabase string `mapstructure:"mysql_database"`
+}
+
+func New() *Config {
+	viper := enviper.New(viper.New())
+
+	// initialize config
+	viper.SetConfigName(".apicfg")
+	viper.AddConfigPath("../..")
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("GOREST")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err == nil {
+		viper.WatchConfig()
+	}
+
+	viper.SetDefault("production", false)
+	viper.SetDefault("log_level", "info")
+	viper.SetDefault("base_url", "http://127.0.0.1/v1")
+	viper.SetDefault("port", "8080")
+	viper.SetDefault("mysql_host", "127.0.0.1:3306")
+	viper.SetDefault("mysql_user", "root")
+	viper.SetDefault("mysql_pass", "")
+	viper.SetDefault("mysql_database", "gorest_db")
+
+	// read config
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		panic(fmt.Sprintf("failed to read config: %s", err.Error()))
+	}
+
+	return &config
+}
---
+++ pkg/go.mod
@@ -0,0 +1,8 @@
+module github.com/Tamplier2911/gorest/pkg
+
+go 1.16
+
+require (
+	github.com/iamolegga/enviper v1.2.1
+	github.com/spf13/viper v1.8.1
+)
